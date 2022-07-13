e6ebd9de38730081a829c2ebabf8f85b58b40722
affanmustafa <affanmustafa2297@gmail.com>
2022-07-13 20:24:11 +0500

    Prisma migration, DTO validation and inline pipes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  signup() {
+  signup(
+    @Body('email') email: string,
+    @Body('password', ParseIntPipe) password: string, // inline pipes are used to parse the body
+  ) {
+    // @Body() is a decorator that tells NestJS to extract the body of the request independent of the framework (Fastify, Express)
+    console.log({
+      email,
+      typeOfEmail: typeof email,
+      password,
+      typeOfPassword: typeof password,
+    });
     return this.authService.signup();
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {} // inject the Prisma service
+
   signup() {
     return { msg: 'Signup success' };
   }
